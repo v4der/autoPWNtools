bf0cfce5777712a0733da6806ab82ce369bb1fc9
v4der <v4der@localhost.localdomain>
2018-04-21 02:00:40 +0200

    --live-hosts

---
+++ c0bra.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+from colored import fg, attr
+import netifaces
+import nmap
+import sys
+
+global QUESTION_ICON
+global GREEN_ICON
+global BLUE_ICON
+global RED_ICON
+global GATEWAY
+global NM
+
+
+
+QUESTION_ICON = "%s[?]%s" % (fg(12) , attr(0))
+GREEN_ICON    = "%s[+]%s" % (fg(172), attr(0))
+BLUE_ICON     = "%s[*]%s" % (fg(56) , attr(0))
+RED_ICON      = "%s[!]%s" % (fg(1)  , attr(0))
+GATEWAY       = netifaces.gateways()["default"][netifaces.AF_INET][0]
+
+def live_hosts():
+	print("%s LIVE HOSTS SCANNING STARTED") % BLUE_ICON
+	print("%s GATEWAY    : %s")             % (BLUE_ICON, GATEWAY)
+	print("%s LIVE HOSTS : ")               % BLUE_ICON   
+	
+	all_network = GATEWAY + "/24"
+	nm          = nmap.PortScanner()
+	
+	nm.scan(hosts = all_network, arguments = "-sP")
+	for host in nm.all_hosts():
+		print("\t%s %s \t%s") % (GREEN_ICON, host, nm[host].hostname())
+
+
+def help():
+	print("""
+%s ARGUMENTS:
+1) --live-hosts                           = scan network for live hosts
+2) --arp-poisoning <TARGET> <PLUGIN>      = arp poisoning attack
+3) --evil-twin <NETWORK_SSID> <INTERFACE> = evil twin attack
+4) --rediect-flash <target> <maleware>    = rediect to fake flash update site 
+	""") % BLUE_ICON
+
+
+def check_arguments():
+	if sys.argv[1] == "--live-hosts":
+		live_hosts()
+		
+	else:
+		print("%s ERROR BAD ARGS") % RED_ICON
+		help() 
+	
+
+def banner():
+	print("""
+######################################
+#                                    #
+#        %s--- [ c0bra ] ---%s           #
+#                                    #
+#     %sWirless Pentesting tool%s        #
+#                                    #
+#       %sCode by : Blackom412%s         #
+#                                    #
+#          %sVERSION : 0.0.1%s           #
+#                                    #
+######################################
+""") % (fg(1)  , attr(0),
+        fg(56) , attr(0),
+        fg(239), attr(0),
+        fg(125), attr(0))
+
+
+#####################
+### PROGRAM START ###
+#####################
+
+def main():
+	banner()
+
+	if len(sys.argv) < 2:
+		help()
+		
+	else:
+		check_arguments()
+
+if __name__ == "__main__":
+	main()
